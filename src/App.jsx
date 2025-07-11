8523a3c27a3c52ff0701e400df4d4c2da53a9c15
AravinthCM <dayaaravinth@gmail.com>
2025-07-11 21:48:05 +0530

    Fix _redirects file format for Netlify SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-// Pages
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import ChatPage from "./pages/ChatPage";
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/chat" element={<ChatPage />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/chat" element={<ChatPage />} />
+    </Routes>
   );
 };
 
